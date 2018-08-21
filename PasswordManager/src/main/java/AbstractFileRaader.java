55af77d74ff7f241809cdf90b4f781e23b7b3adb
GosiaBakM <machmalgorzata@gmail.com>
2018-08-21 10:03:39 +0200

    Add new branch future/generator
    *class generator added with one type of generator
    *main class added

---
+++ PasswordManager/src/main/java/AbstractFileRaader.java
@@ -0,0 +1,4 @@
+public abstract class AbstractFileRaader implements FileReader {
+
+
+}
---
+++ PasswordManager/src/main/java/generator/Main.java
@@ -0,0 +1,8 @@
+package generator;
+
+public class Main {
+    public static void main(String[] args) {
+        PasswordGenerator generator = new PasswordGenerator(8);
+        System.out.println(generator.passwordGenerating());
+    }
+}
